958cee3f7775f535170247897b3f6f7a56909a6e
Federica Mudu <federica.mudu.1993@gmail.com>
2024-10-03 18:04:32 +0200

    da fixare la modale nel file js dei cocktail

--- cocktail.js
+++ cocktail.js
@@ -64,7 +64,24 @@ fetch('./cocktail.json').then((response)=>response.json()).then((data)=>{
             let div = document.createElement('div');
             
             div.innerHTML= `
-            <p>ciao</p>
+            <div class="modal fade" id="${cocktail.name}" tabindex="-1" aria-labelledby="${cocktail.name}" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="${cocktail.name}Label">${cocktail.name}</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            ${cocktail.ingredients}
+                            <br>
+                            ${cocktail.instructions}
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div> 
             `
             modalWrapper.appendChild(div);
         })
@@ -161,21 +178,3 @@ globalFilter()
 
 
 
-{/* <div class="modal fade" id="${cocktail.name}" tabindex="-1" aria-labelledby="${cocktail.name}" aria-hidden="true">
-<div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-    <div class="modal-content">
-        <div class="modal-header">
-            <h1 class="modal-title fs-5" id="${cocktail.name}Label">${cocktail.name}</h1>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-            ${cocktail.ingredients}
-            <br>
-            ${cocktail.instructions}
-        </div>
-        <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        </div>
-    </div>
-</div>
-</div> */}
